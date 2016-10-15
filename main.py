dbb958590ff93381049fc589bd54fbc1e2b70ab1
wilhelmhb <wilhelmhb@live.fr>
2016-10-15 19:43:08 +0200

    optimize number of requests to the API

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ with open("config.yml") as conf_f:
     USERNAME = conf['Discourse_API_username']
     API_KEY = conf['Discourse_API_key']
 
+client = DiscourseClient(URL, USERNAME, API_KEY)
 list_net_groups = extract()
+list_discourse_members = map(get_external_id, [member['id'] for member in client.users()])
+
+def get_external_id(userid):
+    return client.user_all(userid).get('single_sign_on_record').get('external_id')
 
 def main():
-    client = DiscourseClient(URL, USERNAME, API_KEY)
-
     list_groups = client.groups()
     discourse_groups = {group['name'] : group for group in list_groups}
 
@@ -26,11 +29,11 @@ def main():
         if discourse_groups.get(net_group_name, None) is None:
             new_group = client.create_group(net_group_name, "")
             group_id = new_group.get('basic_group').get('id')
-            sync_group(group_id, net_group_name, client)
+            sync_group(group_id, net_group_name)
         else:
-            sync_group(discourse_groups[net_group_name].get('id'), net_group_name, client)
+            sync_group(discourse_groups[net_group_name].get('id'), net_group_name)
 
-def sync_group(group_id, group_name, client):
+def sync_group(group_id, group_name):
     old_members = client.group_members(group_name)
     discourse_members = [member['id'] for member in old_members]
     net_members = list_net_groups[group_name]
@@ -38,11 +41,8 @@ def sync_group(group_id, group_name, client):
 
     if net_members is not None:
         for member in net_members:
-            try:
+            if member in list_discourse_members:
                 member_id = client.by_external_id(member)['id']
-            except DiscourseClientError:
-                member_id = 0
-            if member_id != 0:
                 if member_id in discourse_members:
                     discourse_members.remove(member_id)
                 else:
