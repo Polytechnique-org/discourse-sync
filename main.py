cb4e03307daff79a7e4d0c46ea41414f5d306ba1
Pierre-Alain Dupont <pad@melix.net>
2016-10-15 23:08:35 +0200

    Less requests and some bugfixes

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ with open("config.yml") as conf_f:
 
 client = DiscourseClient(URL, USERNAME, API_KEY)
 list_net_groups = extract()
-list_discourse_members = []
+all_discourse_members = {}
 
 for member in client.users():
     c = client.user_all(member['id']).get('single_sign_on_record', None)
     if c is not None:
-        list_discourse_members.append(c.get('external_id'))
+        all_discourse_members[c.get('external_id')] = c.get('id')
 
 def main():
     list_groups = client.groups()
@@ -40,20 +40,24 @@ def main():
                 sync_group(discourse_groups[net_group_name].get('id'), net_group_name)
 
 def sync_group(group_id, group_name):
-    old_members = client.group_members(group_name)
+    if group_id is None:
+        old_members = []
+    else:
+        old_members = client.group_members(group_name)
     discourse_members = [member['id'] for member in old_members]
     net_members = list_net_groups[group_name]
     l = []
 
     if net_members is not None:
         for member in net_members:
-            if member in list_discourse_members:
-                member_id = client.by_external_id(member)['id']
+            if member in all_discourse_members:
+                member_id = all_discourse_members[member]
                 if member_id in discourse_members:
                     discourse_members.remove(member_id)
                 else:
                     if group_id is None:
                         group_id = client.create_group(group_name, "").get('basic_group').get('id')
+                        print("Created group '{0}'".format(group_name))
                     member = client.add_user_to_group(group_id, member_id)
                 l.append(member_id)
 
